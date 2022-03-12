135d850c62569671fe609f00686d52083de2fc0e
Vaibhav Choudhary <vaibhavch29@gmail.com>
2022-03-12 16:57:58 +0530

    removed no input

--- hello.py
+++ hello.py
@@ -5,7 +5,5 @@ st.title("Streamlit Web-App!")
 name = st.text_input("Enter your name:")
 msg = "Hello {}!".format(name)
 
-number = st.number_input("Enter a no:", 1, 5)
-
 if name:
     st.write(msg)
