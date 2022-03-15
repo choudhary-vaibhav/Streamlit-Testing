ba445e5b324a63996c5a1b46b1c5cd8720195610
Vaibhav Choudhary <vaibhavch29@gmail.com>
2022-03-15 20:21:39 +0530

    testing

--- new.py
+++ new.py
@@ -1,8 +1,17 @@
 import streamlit as st
-
-st.header("Hello World!")
-
-num = st.number_input("Enter a num:", 1, 10)
-
-if num:
-    st.write(num)
+import requests
+from PIL import Image
+from validators.url import url
+st.title('Kanna Says!!')
+text = st.text_input("What do you want kanna to say?\n")
+if text == '':
+    st.write("Type something!!")
+else:
+    r = requests.get(f"https://nekobot.xyz/api/imagegen?type=kannagen&text={text}"
+).json()
+    a = r.get("message")
+    iurl = url(a)
+    with open("temp.png", "wb") as f:
+        f.write(requests.get(a).content)
+    img = Image.open("temp.png").convert("RGB")
+    st.image(img)
